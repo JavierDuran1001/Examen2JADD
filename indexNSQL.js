54f7351dd2c43cf22ab97c3e6018bde86b20f89e
JavierDuran1001 <a01747783@itesm.mx>
2021-10-22 00:19:55 -0500

    Eliminados archivos innecesarios

--- indexNSQL.js
+++ indexNSQL.js
@@ -1,12 +1,10 @@
 const express = require("express")
 const mongoose = require("mongoose")
-const pokemonRoutes = require("./routes/pokemon")
 const ciudadesRoutes = require("./routes/ciudades")
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use("/pokemon",pokemonRoutes)
 app.use("/ciudades",ciudadesRoutes)
 
 mongoose.connect('mongodb://user3:root@18.234.222.26:27017/base3?authSource=admin')
--- routes/pokemon.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router()
-const pokemonController = require("../controllers/pokemon")
-
-router.post('/agregarCiudades',pokemonController.postAgregarPokemon)
-router.get('/obtenerPokemon',pokemonController.getObtenerPokemon)
-router.post('/actualizarPokemon',pokemonController.postActualizarPokemon)
-router.post('/borrarPokemon',pokemonController.postBorrarPokemon)
-
-module.exports = router
